492c15f7b526019a0e5e29bf2b511a30571e5091
Nezarik-Intmax <nezarikintmax@gmail.com>
2022-05-28 08:14:59 +0500

    h

--- OS_lab2_/Form1.Designer.cs
+++ OS_lab2_/Form1.Designer.cs
@@ -29,68 +29,71 @@ namespace OS_lab2_ {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.InputBox = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(588, 152);
+            this.richTextBox1.Location = new System.Drawing.Point(387, 12);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 231);
+            this.richTextBox1.Size = new System.Drawing.Size(401, 426);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(482, 152);
+            this.richTextBox2.Location = new System.Drawing.Point(12, 12);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(100, 231);
+            this.richTextBox2.Size = new System.Drawing.Size(218, 231);
             this.richTextBox2.TabIndex = 1;
             this.richTextBox2.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(294, 287);
+            this.button1.Location = new System.Drawing.Point(236, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(145, 55);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Первый поток";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(294, 317);
+            this.button2.Location = new System.Drawing.Point(236, 73);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(145, 55);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "Второй поток";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(294, 347);
+            this.button3.Location = new System.Drawing.Point(236, 134);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(145, 55);
             this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
+            this.button3.Text = "Синхронная работа потоков";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
             // 
-            // InputBox
+            // button4
             // 
-            this.InputBox.Location = new System.Drawing.Point(104, 287);
-            this.InputBox.Name = "InputBox";
-            this.InputBox.Size = new System.Drawing.Size(100, 96);
-            this.InputBox.TabIndex = 5;
-            this.InputBox.Text = "";
+            this.button4.Location = new System.Drawing.Point(236, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(145, 55);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Очистить вывод";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -109,7 +112,7 @@ namespace OS_lab2_ {
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.RichTextBox InputBox;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- OS_lab2_/Form1.cs
+++ OS_lab2_/Form1.cs
@@ -11,8 +11,41 @@ using System.Windows.Forms;
 
 namespace OS_lab2_ {
     public partial class Form1 : Form {
+        Task t1;
+        Task t2;
+        Task t3;
+        Task t1_sync;
+        Task t2_sync;
+        RichTextBox textBox_main;
+        RichTextBox textBox;
+
+        public delegate void del();
+
+        int[] array = new int[10];
+        bool sort1 = false;
+        bool sort2 = false;
+        private readonly object locker = new object();
+
         public Form1() {
             InitializeComponent();
+            t1 = new Task(Bubble);
+            t2 = new Task(BackBubble);
+            t3 = new Task(APrint);
+            t1_sync = new Task(BubbleSync);
+            t2_sync = new Task(BackBubbleSync);
+
+            textBox_main = this.richTextBox1;
+            textBox = this.richTextBox2;
+
+            Random random = new Random();
+            string str = "";
+            for (int i = 0; i < 10; i++) {
+                array[i] = random.Next(-41, 41);
+                str += ($"{array[i]} ");
+            }
+            str += "\n";
+            textBox.Text += str;
+            t3.Start();
         }
         int[] bubbleSortMin(int[] _arr, int N) {
             for (int i = 0; i < N; i++) {
@@ -44,7 +77,9 @@ namespace OS_lab2_ {
         }
 
         private void button1_Click(object sender, EventArgs e) {
-            string[] tmp = InputBox.Text.Split(' ');
+            if (t1.Status != TaskStatus.Running)
+                t1.Start();
+            /*string[] tmp = InputBox.Text.Split(' ');
             int[] arr = new int[tmp.Length];
             for (int i = 0; i < tmp.Length; i++) {
                 arr[i] = Convert.ToInt32(tmp[i]);
@@ -53,11 +88,13 @@ namespace OS_lab2_ {
             richTextBox2.Clear();
             for (int i = 0; i < arr.Length; i++) {
                 richTextBox2.Text += arr[i].ToString() + '\n';
-            }
+            }*/
         }
         int[] arr;
         private void button2_Click(object sender, EventArgs e) {
-            Thread thread1 = new Thread(threadFunc1);
+            if (t2.Status != TaskStatus.Running)
+                t2.Start();
+            /*Thread thread1 = new Thread(threadFunc1);
             Thread thread2 = new Thread(threadFunc2);
             string[] tmp = InputBox.Text.Split(' ');
             if(arr != null) {
@@ -67,7 +104,7 @@ namespace OS_lab2_ {
             for (int i = 0; i < tmp.Length; i++) {
                 arr[i] = Convert.ToInt32(tmp[i]);
             }
-            thread1.Start();
+            thread1.Start();*/
         }
         void printArr(int[] _arr) {
 
@@ -83,5 +120,109 @@ namespace OS_lab2_ {
         }
         static void threadFunc2() { }
         //this.richTextBox1.BeginInvoke((MethodInvoker)(() => this.richTextBox1.Text += "text"));
+        public void Print() {
+            string str = "";
+            for (int i = 0; i < array.Length; i++) {
+                str += ($"{array[i]} ");
+            }
+            str += "\n";
+            textBox_main.Invoke(new del(() => textBox_main.Text += str));
+        }
+
+        public void Bubble() {
+            int t = 0;
+            for (int p = 0; p <= array.Length - 2; p++) {
+                for (int i = 0; i <= array.Length - 2; i++) {
+                    if (array[i] > array[i + 1]) {
+                        lock (locker) {
+                            t = array[i + 1];
+                            array[i + 1] = array[i];
+                            array[i] = t;
+                            sort1 = true;
+                            Thread.Sleep(50);
+                            //Print();
+                        }
+                    }
+                }
+            }
+        }
+
+        public void BackBubble() {
+            int t = 0;
+            for (int p = 0; p <= array.Length - 2; p++) {
+                for (int i = 0; i <= array.Length - 2; i++) {
+                    if (array[i] < array[i + 1]) {
+                        lock (locker) {
+                            t = array[i + 1];
+                            array[i + 1] = array[i];
+                            array[i] = t;
+                            sort2 = true;
+                            Thread.Sleep(50);
+                            //Print();
+                        }
+                    }
+                }
+            }
+        }
+
+        public void BubbleSync() {
+            int t = 0;
+            lock (locker) {
+                for (int p = 0; p <= array.Length - 2; p++) {
+                    for (int i = 0; i <= array.Length - 2; i++) {
+                        if (array[i] > array[i + 1]) {
+                            t = array[i + 1];
+                            array[i + 1] = array[i];
+                            array[i] = t;
+                            sort1 = true;
+                            Thread.Sleep(10);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public void BackBubbleSync() {
+            int t = 0;
+            lock (locker) {
+                for (int p = 0; p <= array.Length - 2; p++) {
+                    for (int i = 0; i <= array.Length - 2; i++) {
+                        if (array[i] < array[i + 1]) {
+                            t = array[i + 1];
+                            array[i + 1] = array[i];
+                            array[i] = t;
+                            sort2 = true;
+                            Thread.Sleep(10);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void APrint() {
+            while (true) {
+                if (sort1 == true || sort2 == true) {
+                    string text = sort1 == true ? "[1]:" : "[2]:";
+                    textBox_main.Invoke(new del(() => textBox_main.Text += text));
+                    Print();
+                    sort1 = false;
+                    sort2 = false;
+                }
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e) {
+            if (t1.Status != TaskStatus.Running && t2.Status != TaskStatus.Running) {
+                t1_sync.Start();
+                t2_sync.Start();
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+
+        }
     }
 }
+
